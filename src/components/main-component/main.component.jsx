dac544ab9aecbf37dbe29758d89d0716b2e3b93e
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-08 21:53:12 +0100

    done with the main component place at the middle of the home page

--- src/components/main-component/main.component.jsx
+++ src/components/main-component/main.component.jsx
@@ -1,9 +1,40 @@
 import "./main.style.css";
+import IconUser from "../../images/user.svg";
+import IconPhoto from "../../images/image-solid-full.svg";
+import IconVideo from "../../images/video-solid-full.svg";
+import IconEvent from "../../images/calendar-solid-full.svg";
+import IconArticle from "../../images/newspaper-solid-full.svg";
 
 const MainComponent = (props) => {
   return (
     <div className="main-container">
-      <div className="share-box">share</div>
+      <div className="common-card">
+        <div className="share-box">
+          Share
+          <div className="user-profile">
+            <img className="user" src={IconUser} alt="" />
+            <button>Start a post</button>
+          </div>
+          <div className="span-block">
+            <button>
+              <img src={IconPhoto} alt="" />
+              <span>Photo</span>
+            </button>
+            <button>
+              <img src={IconVideo} alt="" />
+              <span>Video</span>
+            </button>
+            <button>
+              <img src={IconEvent} alt="" />
+              <span>Event</span>
+            </button>
+            <button>
+              <img src={IconArticle} alt="" />
+              <span>article</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
