01cc4e4a4411b5d472a3d153ca74f12b9bedc9e5
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-08 15:42:58 +0100

    done with the right component place at the home page

--- src/components/main-component/main.component.jsx
+++ src/components/main-component/main.component.jsx
@@ -1,6 +1,10 @@
 import "./main.style.css";
 
 const MainComponent = (props) => {
-  return <div className="main-container">Main</div>;
+  return (
+    <div className="main-container">
+      <div className="share-box">share</div>
+    </div>
+  );
 };
 export default MainComponent;
--- src/components/right-component/right.component.jsx
+++ src/components/right-component/right.component.jsx
@@ -1,6 +1,47 @@
 import "./right.style.css";
+import IconFeed from "../../images/feed-icon.svg";
+import IconRight from "../../images/right-icon.svg";
 
 const RightComponent = (props) => {
-  return <div className="right-container">Right</div>;
+  return (
+    <div className="right-container">
+      <div className="overall-block">
+        <div className="title">
+          <h2>Add to your feed</h2>
+          <img src={IconFeed} alt="" />
+        </div>
+        <ul className="feed-list">
+          <li>
+            <a>
+              <div className="avater"></div>
+            </a>
+            <div>
+              <span>Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <div className="avater"></div>
+            </a>
+            <div>
+              <span>Evidence</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </ul>
+        <a className="recommendation">
+          View all recommendation
+          <img src={IconRight} alt="" />
+        </a>
+      </div>
+      <div className="banner-card">
+        <img
+          src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 export default RightComponent;
