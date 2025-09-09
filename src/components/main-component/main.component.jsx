6e266bac83b9bc93ec83b514476a630bb31326f5
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-09 06:35:14 +0100

    given the main component artice with some nice icons

--- src/components/main-component/main.component.jsx
+++ src/components/main-component/main.component.jsx
@@ -4,6 +4,15 @@ import IconPhoto from "../../images/image-solid-full.svg";
 import IconVideo from "../../images/video-solid-full.svg";
 import IconEvent from "../../images/calendar-solid-full.svg";
 import IconArticle from "../../images/newspaper-solid-full.svg";
+import IconEllipsis from "../../images/ellipsis-solid-full.svg";
+import Img from "../../images/img.jpg";
+import IconClapp from "../../images/hands-clapping-solid-full.svg";
+import IconLike from "../../images/thumbs-up-solid-full.svg";
+import { FaRegCommentDots } from "react-icons/fa";
+import { FaShare } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io";
+import { BiLike } from "react-icons/bi";
+import { FaHandsClapping } from "react-icons/fa6";
 
 const MainComponent = (props) => {
   return (
@@ -32,6 +41,61 @@ const MainComponent = (props) => {
               <img src={IconArticle} alt="" />
               <span>article</span>
             </button>
+          </div>
+        </div>
+        <div>
+          <div className="article">
+            <div className="shared-actor">
+              <a>
+                <img className="user-post" src={IconUser} alt="" />
+                <div>
+                  <span>Title</span>
+                  <span>Info</span>
+                  <span>Data</span>
+                </div>
+              </a>
+              <button className="button-icon">
+                <img src={IconEllipsis} alt="" />
+              </button>
+            </div>
+            <div className="description">Description</div>
+            <div className="shared-img">
+              <a>
+                <img src={Img} alt="" />
+              </a>
+            </div>
+            <div className="social-count">
+              <li>
+                <button>
+                  {/* <img src={IconLike} alt="" /> */}
+                  <BiLike className="like" />
+                  {/* <img src={IconClapp} alt="" /> */}
+                  <FaHandsClapping />
+                  <span>75</span>
+                </button>
+              </li>
+              <li>
+                <a>2 comments</a>
+              </li>
+            </div>
+            <div className="social-icon">
+              <button>
+                <BiLike />
+                <span>Like</span>
+              </button>
+              <button>
+                <FaRegCommentDots />
+                <span>Comments</span>
+              </button>
+              <button>
+                <FaShare />
+                <span>Share</span>
+              </button>
+              <button>
+                <IoIosSend />
+                <span>Send</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
